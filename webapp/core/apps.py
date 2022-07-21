e1fe806eabb8b9417fb953507322b028f426cf51
Mark Ruddy <1markruddy@gmail.com>
2022-07-21 14:20:04 +0100

    add initial django setup with bootstrap and core app

--- webapp/core/apps.py
+++ webapp/core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PoolsConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pools'
+    name = 'core'
