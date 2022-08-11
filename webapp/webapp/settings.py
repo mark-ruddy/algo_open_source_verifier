2ec2547efeb9edc4250986bb4d5325988203a674
Mark Ruddy <1markruddy@gmail.com>
2022-08-11 16:33:57 +0100

    add localhost allowed host

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-_uz@zc=u#cb^ikrp-=djy4k0$$ri*177(+x6ua**c3vb6-am+1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
